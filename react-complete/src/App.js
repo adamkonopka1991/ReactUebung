5d6609c2bfc9116cb7ea10d902bf97396a8a50c1
Adam <adam.konopka9@outlook.de>
2018-12-22 11:48:34 +0100

    After finishing Module Error Handling

--- react-complete/src/App.js
+++ react-complete/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import classes from './App.module.css';
 
 import Person from './Person/Person.js';
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 
 
 class App extends Component {
@@ -49,14 +50,14 @@ class App extends Component {
 
   render() {
   
-    const style={
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer'
-    };//Achtung: Pseudo-Selektoren können nicht verwendet werden!Deswegen: Radium
+    //css Klasse für Button
+    let btnClass= "";
+    // const rnd= Math.random();
+    // if(rnd<0.7)
+    // {
+    //   throw new Error('Something went wrong');
+    // }
+
 
     //Persons
     let persons= null;
@@ -65,17 +66,18 @@ class App extends Component {
         <div>
 
           {this.state.persons.map((person, index) => {
-            return <Person
+            return <ErrorBoundary key={person.id}/* key has to be in the outer Element*/><Person
               click={() => this.deletePersonHandler(index)}
               name={person.name}
               age={person.age} 
               key={person.id} //notwendig, sodass React effizienter arbeiten kann
-              changed={(event) => this.nameChangedHandler(event, person.id)} /> //so react does not rerender the whole list due to a change, it rerenders just the changed element
+              changed={(event) => this.nameChangedHandler(event, person.id)} /></ErrorBoundary> //so react does not rerender the whole list due to a change, it rerenders just the changed element
           })/*Umwandlung des persons-Array in ein Array aus JSX-Objekten*/}
         </div>
       );
 
-      style.backgroundColor= 'red';
+      btnClass= classes.Red;
+      
     }
 
 
@@ -93,9 +95,9 @@ class App extends Component {
       <div className={classes.App}>
         <h1>Hi, I'm a React App</h1>
         <p className={assignedClasses.join(' ')}>This is really working!</p>
-        <button 
-        onClick={this.togglePersonsHandler}
-        style={style}>Toggle Persons</button> {/*<!-- Function returns a function call;not executed immediately! we pass an anonymus function;*/}
+        <button
+        className={btnClass} 
+        onClick={this.togglePersonsHandler}>Toggle Persons</button> {/*<!-- Function returns a function call;not executed immediately! we pass an anonymus function;*/}
         
         
         {persons}

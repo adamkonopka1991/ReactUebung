69c8f48bcb4ee482f9d94c332ed68f77bc89a6d5
Adam <adam.konopka9@outlook.de>
2018-12-30 19:41:47 +0100

    Before installing prop-types

--- react-complete/src/Containers/App.js
+++ react-complete/src/Containers/App.js
@@ -18,7 +18,8 @@ class App extends PureComponent {
         {id: 'sss', name: 'Manu', age: 29},
         {id: 'as', name: 'Stephanie', age: 26}
       ],
-      showPersons: false
+      showPersons: false,
+      toggleClicked: 0
     }
   }
 
@@ -82,7 +83,12 @@ class App extends PureComponent {
   togglePersonsHandler =() => 
   {
     const doesShow= this.state.showPersons;
-    this.setState({showPersons: !doesShow});
+    this.setState((prevState,props) => {
+      return {
+        showPersons: !doesShow,
+        toggleClicked: prevState.toggleClicked +1
+      }
+    });
   }
 
   render() {

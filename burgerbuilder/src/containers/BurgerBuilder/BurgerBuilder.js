51e8afaff195b9bb4a493219859daee2744a5fc4
Adam <adam.konopka9@outlook.de>
2019-01-11 22:33:55 +0100

    After Lecture 133

--- burgerbuilder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burgerbuilder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -98,6 +98,11 @@ class BurgerBuilder extends Component {
         this.setState({purchasing: false});
     }
 
+    purchaseContinueHandler= () =>
+    {
+        alert("You continue!");
+    }
+
     render(){
 
         //Check if Less-Butten should be disabled or enabled
@@ -113,7 +118,10 @@ class BurgerBuilder extends Component {
         return (
             <Aux>
                 <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
-                    <OrderSummary ingredients={this.state.ingredients} />
+                    <OrderSummary 
+                        ingredients={this.state.ingredients}
+                        purchaseCancelled={this.purchaseCancelHandler}
+                        purchaseContinued={this.purchaseContinueHandler} />
                 </Modal>
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls
